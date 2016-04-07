218f8ca1cc9ef86d40a575990bc070897fd3143c
Afton Downey and Bri Popson <aaike001@gmail.com>
2016-04-07 13:20:10 -0700

    show items under correct categories

--- app/models/category.js
+++ app/models/category.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   type: DS.attr(),
   image: DS.attr(),
-  // items: DS.hasMany('item', {async: true})
+  items: DS.hasMany('item', {async: true})
 });
