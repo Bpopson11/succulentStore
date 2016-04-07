24c46fb7dff70dc93cd6c3f980ae1eac7810e7f3
Afton Downey and Bri Popson <aaike001@gmail.com>
2016-04-07 15:17:13 -0700

    have correct Add to Cart functionality

--- app/components/display-cart.js
+++ app/components/display-cart.js
@@ -2,4 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
+
+  cartTotal: Ember.computed('shoppingCart.items.[]', function(){
+    var total = 0;
+    for (var i = 0; i < this.get.shoppingCart.items.length; i++) {
+      total += this.get('shoppingCart.items')[i].get('price');
+    }
+    return total;
+  })
 });
